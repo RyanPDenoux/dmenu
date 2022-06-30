5f83136c1ad3422268ea34d2d5c021d8c1abe287
Ryan Denoux <ryanpdenoux@gmail.com>
2022-06-30 11:11:48 +0200

    change selection color for visibility

--- config.h
+++ config.h
@@ -5,15 +5,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mono:size=12",
-	"Noto Color Emoji:size=12"
+	"monospace:size=12:autohint=true",
+	"Twemoji:pixelsize=12:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#586e75", "#073642" },
-	[SchemeSel] = { "#93a1a1", "#002b36" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#eee8d5", "#268bd2" },
+	[SchemeOut] = { "#073642", "#2aa198" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
